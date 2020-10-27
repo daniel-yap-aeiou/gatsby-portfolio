80244f4c36cde441e866609745b7f5721de52730
Daniel Yap <danielytf@gmail.com>
2020-10-27 15:13:17 +1100

    edit

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   //manifestIcon: 'src/assets/img/favicon.png',
-  pathPrefix: `/gatsby-portfolio/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Daniel',
   lastName: 'Yap',
   // social
